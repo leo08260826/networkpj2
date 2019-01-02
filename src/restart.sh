4e96b419af7344f2a64794699dca2a71195d3986
廖威仲 <michael07031224@gmail.com>
2019-01-02 17:15:38 +0800

    final_final

--- src/restart.sh
+++ src/restart.sh
@@ -3,5 +3,7 @@
 # scp -r ../src b05902059@linux6.csie.org:./ComputerNetwork/
 rm -rf USER
 mkdir USER
-rm -f Account/account.txt
+rm -rf Account
+# rm -f Account/account.txt
+mkdir Account
 touch Account/account.txt
